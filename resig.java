a95f694fd90f709997d1eabebfe6ad8ac5334379
harish123 <harish123@gamil.com>
2022-04-13 17:51:08 +0000

    aP details

--- resig.java
+++ resig.java
@@ -11,4 +11,4 @@ what r u doing now
 whatspp goong on
 it wioll be a workfrom office or work from home
 
-
+Andhra Pradesh (English: /ˌɑːndrə prəˈdɛʃ/,[11] Telugu: [ãːndʱrʌ prʌdeːɕ]; audio speaker iconlisten (help·info)) is a state in the south-eastern coastal region of India.[12] It is the seventh-largest state by area covering an area of 162,975 km2 (62,925 sq mi)[4] and tenth-most populous state with 49,386,799 inhabitants,[13][14] with the state capital being Amaravati and Largest city in the state is Visakhapatnam.[15] It is bordered by Telangana to the north-west, Chhattisgarh to the north, Odisha to the north-east, Tamil Nadu to the south, Karnataka to the west and the Bay of Bengal to the east.[16] It has the second longest coastline in India after Gujarat, of about 974 km (605 mi).[17] Andhra State was the first state to be formed on a linguistic basis in India on 1 October 1953.[18] On 1 November 1956, Andhra State was merged with the Telugu-speaking areas of the Hyderabad State to form Andhra Pradesh. The state was then reorganised in June 2014 to form a separate Telangana state.
