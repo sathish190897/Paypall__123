de4d2b7912f8f3b24bf4d8187f092ec0cc9353d1
harish123 <harish123@gamil.com>
2022-04-13 17:42:52 +0000

    version2

--- resig.java
+++ resig.java
@@ -2,3 +2,13 @@ hi
 how r u
 how is doing 
 how is going ur office
+
+
+who r u
+when will be available for interview
+when r u coming to hyderabad
+what r u doing now
+whatspp goong on
+it wioll be a workfrom office or work from home
+
+
